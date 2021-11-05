97b8f9957941f945ea76a7a779a9f45eed535f2d
wesley_wisch <wesley_wisch@hotmail.com>
2021-11-05 10:55:01 -0300

    Skills pronto, ja passando as props para o modal e abrindo ele

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -5,33 +5,54 @@ import { Container, Card } from './styles';
 import { Modal } from "../Modal";
 import { Title } from "../Title";
 
-export function Portfolio(){
-  const [modalOpen, setModalOpen] = useState(false);
+import { PortfolioJson } from '../../json/Portfolio';
 
-  function openModal() {
+type Props = {
+  id: number,
+  img1: string,
+  img2: string,
+  img3: string,
+  paragrafo: string,
+  linkRepo: string,
+  linkOnline: string,
+}
+
+export function Portfolio() {
+  let item3: Props[] = PortfolioJson;
+  
+  const [modalOpen, setModalOpen] = useState(false);
+  const [item2, setItem2] = useState<Props[]>([]);
+
+
+  function openModal(id: number) {
     setModalOpen(true);
+    let itemFiltered = item3.filter(i => i.id === id);
+    setItem2(itemFiltered);
   }
 
   function closeModal() {
     setModalOpen(false);
   }
 
-  return(
+  return (
     <Container id="portfolio">
-      <Title 
+      <Title
         title="Portfólio"
         fontSize={3}
-        size="rem" 
+        size="rem"
         BorderColor="#fff"
         BorderSize={15}
       />
 
-      <Card 
-        onClick={openModal} 
-        src="https://www.casasbahia-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1403647406" 
-      />
+      {PortfolioJson.map((item, key) => (
+        <Card
+          key={key}
+          onClick={() => openModal(item.id)}
+          src={item.img1}
+        />
+      ))}
 
-      {modalOpen && <Modal modalOpen={modalOpen} closeModal={closeModal} />}
+      {modalOpen && <Modal modalOpen={modalOpen} closeModal={closeModal} data={item2[0]} />}
     </Container>
   );
 }
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import { Title } from '../Title';
 
-import { Container } from './styles';
+import { Container, TechContainer, Tech, Progress, BarraProgress, NameTech, Paragrafo } from './styles';
+
+import Skill from '../../images/skills.png';
+
+import { Techs } from '../../json/Techs';
 
 export function Skills() {
-  return(
+  return (
     <Container id="skills">
-      <Title 
+      <Title
         title="Skills"
+        color="#333"
         fontSize={3}
         size="rem"
-        BorderColor="#fff"
+        BorderColor="#333"
         BorderSize={15}
       />
+
+      <div>
+        <TechContainer>
+          {Techs.map((tech, key) => {
+            const { icon: Icon } = tech;
+
+            return (
+              <Tech key={key}>
+                <NameTech>
+                  <Paragrafo className="name">
+                    <Icon
+                      size={24}
+                      className="icon"
+                      color={tech.color}
+                    />
+                    {tech.techName}
+                  </Paragrafo>
+                  <Paragrafo>{tech.TechPercentage}%</Paragrafo>
+                </NameTech>
+                <Progress>
+                  <BarraProgress style={{ width: `${tech.TechPercentage}%` }}></BarraProgress>
+                </Progress>
+              </Tech>
+            )
+          })}
+        </TechContainer>
+
+        <img src={Skill} alt="" />
+      </div>
     </Container>
   );
 }
