b2a62b100192cb68a82408f272801a5eae9d5457
wesley_wisch <wesley_wisch@hotmail.com>
2022-03-01 15:33:57 -0300

    Feture começando a implemntar os teste automatizados

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -14,7 +14,7 @@ export function Experience() {
       <section>
         <ExperienceCard
           year={2021}
-          title="Certificado do termino de HTML | CSS | Java Script"
+          title="Certificado do curso de HTML | CSS | Java Script"
           description='
             Essa foi a minha primeira jornada em um curso aonde eu aprendi com o professor Igor, HTML, CSS e Javascript.
           '
@@ -24,7 +24,7 @@ export function Experience() {
 
         <ExperienceCard
           year={2021}
-          title="Certificado do termino de Firebase | jQuery | Bootstrap"
+          title="Certificado do curso de Firebase | jQuery | Bootstrap"
           description='
             Indo mais afundo aprendi um dos banco de dados noSql(firebase), um framework CSS(Bootstrap).
           '
@@ -34,7 +34,7 @@ export function Experience() {
 
         <ExperienceCard
           year={2021}
-          title="Certificado do termino NodeJs | MongoDB"
+          title="Certificado do curso de NodeJs | MongoDB"
           description='
             Indo rumo ao Back-end aprendi NodeJs com o padrão MVC com ejs. e também construindo uma api com NodeJs e como banco de dados o MongoDB.
           '
@@ -44,7 +44,7 @@ export function Experience() {
 
         <ExperienceCard
           year={2021}
-          title="Certificado do termino da Trilha ReactJs"
+          title="Certificado do curso da Trilha ReactJs"
           description='
             Voltando ao Front-end, aprendo mais e mais sobre esse mundo com o React, onde tivemos teste unitários, integração com api de pagamento e muito mais.
           '
---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
+
+import { ThemeProvider } from "styled-components";
+import { colors } from "../../styles/themes/colors";
+
+import { Header } from "./index";
+
+describe('Component header', () => {
+  it('Check if the names are in the html and are displayed on screen', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={colors}>
+          <Header toggle={() => false} />
+        </ThemeProvider>
+      </MemoryRouter>
+    );
+
+    expect(getByText('Wesley Wisch')).toBeInTheDocument();
+    expect(screen.getByText('Wesley Wisch')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Projetos')).toBeInTheDocument();
+  });
+});
